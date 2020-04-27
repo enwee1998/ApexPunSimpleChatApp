404da631c01903989ccca2990926975e9dd285b4
Weerayut Thinchamlong <weerayut.thc@gmail.com>
2020-04-27 14:02:05 +0700

    Implement users collection and save user function

--- server/socketManager.js
+++ server/socketManager.js
@@ -1,49 +1,57 @@
-const io = require('./index.js').io;
+const io = require("./index.js").io;
+const mongoose = require("mongoose");
+const User = require("./models/User");
 
-const {VERIFY_USER, USER_CONNECTED, LOGOUT} = require('../src/Communicate');
-const {createUser, createMessage, createChat} = require('../src/manageChat');
+const { VERIFY_USER, USER_CONNECTED, LOGOUT } = require("../src/Communicate");
+const { createUser, createMessage, createChat } = require("../src/manageChat");
 let connectedUsers = {};
 
-module.exports = function(socket){
-    console.log("Socket ID :" + socket.id);
+module.exports = function (socket) {
+  console.log("Socket ID :" + socket.id);
 
-    // verify username
-    socket.on(VERIFY_USER, (username, callback)=>{
-      if(isUser(connectedUsers,username)){
-        callback({isUser:true, user:null});
-      }
-      else{
-        callback({isUser:false, user:createUser({name:username})});
-      }
-    })
+  // verify username
+  socket.on(VERIFY_USER, (username, callback) => {
+    if (isUser(connectedUsers, username)) {
+      callback({ isUser: true, user: null });
+    } else {
+      callback({ isUser: false, user: createUser({ name: username }) });
+    }
+  });
 
-    //user connects
-    socket.on(USER_CONNECTED, (user)=>{
-      connectedUsers = addUser(connectedUsers, user);
-      socket.user = user;
+  //user connects
+  socket.on(USER_CONNECTED, (user) => {
+    connectedUsers = addUser(connectedUsers, user);
+    socket.user = user;
 
-      io.emit(USER_CONNECTED, connectedUsers);
-      console.log(connectedUsers);
-    })
+    io.emit(USER_CONNECTED, connectedUsers);
+    console.log(connectedUsers);
+  });
 
-    //user disconnects
+  //user disconnects
 
+  //logout
+};
 
-    //logout
-}
-
-function addUser(userList, user){
-  let newList = Object.assign({},userList);
+function addUser(userList, user) {
+  let newList = Object.assign({}, userList);
   newList[user.name] = user;
+  User.find({ name: user.name }, function (err, users) {
+    if (err) throw err;
+    if (!users.length) {
+      var newUser = new User({ name: user.name });
+      newUser.save();
+    }
+  });
   return newList;
 }
 
-function removeUser(userList, username){
-  let newList = Object.assign({},userList);
+function removeUser(userList, username) {
+  let newList = Object.assign({}, userList);
   delete newList[username];
   return newList;
-}1
+}
+1;
 
-function isUser(userList, username){
-    return username in userList;
+function isUser(userList, username) {
+  return username in userList;
 }
